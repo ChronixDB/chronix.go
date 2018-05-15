fe8d0370c0e1ca7dfa78e9b09644b7c8598b1699
Stephan Klapproth <stephan.klapproth@qaware.de>
2018-05-15 13:05:05 +0200

    Fixes NaN caused by float overflow

--- chronix/statscalc_test.go
+++ chronix/statscalc_test.go
@@ -1,6 +1,9 @@
 package chronix
 
-import "testing"
+import (
+	"testing"
+	"math"
+)
 
 func TestCalculateStats(t *testing.T) {
 
@@ -34,6 +37,23 @@ func TestCalculateStats(t *testing.T) {
 
 	if stats.timespan != 77 {
 		t.Error("Expected timespan=77, got ", stats.timespan)
+	}
+}
+
+func TestWithBigFloat(t *testing.T) {
+	// given:
+	series := TimeSeries{ Name: "Test", Type: "metric", Attributes: map[string]string{ "host": "node0"}}
+	series.Points = []Point{{Timestamp: 1, Value: math.MaxFloat64}, {Timestamp: 2, Value:math.MaxFloat64}}
+
+	// when:
+	stats, err := calculateStats(&series)
+	// then:
+	if err != nil {
+		t.Fatal("Failed to calculate Stats", err)
+	}
+
+	if stats.avg != math.MaxFloat64 {
+		t.Error("Expected the avg to be the MaxFloat64 value, got ", stats.avg)
 	}
 }
 
